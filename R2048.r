4f84daa0a93987b3c337f9929267584304f1079b
davidseres <davidseres@gmail.com>
2014-03-27 17:46:48 +0100

    R2048.r
    This file starts an instance of the game. User input is called via "readline()" and the mapped keys are displayed in line 11. I chose the "classical" gaming keys for up (w), down (s), right (d) and left (a).

---
+++ R2048.r
@@ -0,0 +1,23 @@
+start_2048 <- function() {
+
+	source("2048_functions.r")
+	
+	initialize()
+	plot_Grid2048()
+	
+	while(!bln_StopCondition) {
+	
+		UI <- readline()
+		UI_trans <- switch(UI, w="up", s="down", a="left", d="right", NULL)
+		if(!is.null(UI_trans)) {
+			step_Move(UI_trans)
+			plot_Grid2048()
+			stop_Condition()
+		}
+	
+	}
+	
+	mtext("Game Over", cex = 2, col = "red")
+	return("Game Over")
+
+}
